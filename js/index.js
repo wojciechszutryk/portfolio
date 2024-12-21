ab5cd15804b88f3b1624dd9aa8a5baf0ef928f38
w.szutryk <w.szutryk@optimumpareto.com>
2024-12-21 14:16:16 +0100

    add webpack to optimize and minify

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import "./certificates.js";
+import "./eduProgress.js";
+import "./headerScroll.js";
+import "./scrollProgress.js";
+import "./sendEmail.js";
+import "./skillList.js";
+import "./skillTabPanels.js";
+import "./typewriter.js";
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./js/index.js",
+    styles: "./css/style.css",
+  },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index_PL.html",
+      filename: "index_PL.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./img",
+          to: "img",
+        },
+        {
+          from: "./js/swiper-bundle.min.js",
+          to: "js/swiper-bundle.min.js",
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+      new HtmlMinimizerPlugin(),
+    ],
+  },
+};
